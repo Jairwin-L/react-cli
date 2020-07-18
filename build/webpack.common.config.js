6d503b25b406e262cfdf1fccc0ec60366d041e2b
Jairwin <jairwin@jairwin.cn>
2020-07-18 13:24:43 +0800

    +

--- build/webpack.common.config.js
+++ build/webpack.common.config.js
@@ -1,6 +1,7 @@
 "use strict"
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const UglifyWebpackPlugin = require('uglifyjs-webpack-plugin');
 const path = require('path')
 const srcDir = path.join(__dirname, "../src");
 console.log(path.join(__dirname, "../src/index.jsx"));
@@ -21,6 +22,14 @@ module.exports = {
     }),
     new CleanWebpackPlugin(),
   ],
+  optimization: {
+    minimizer: [
+      new UglifyWebpackPlugin({
+        exclude: /\/excludes/,
+        parallel: true,
+      }),
+    ],
+  },
   module: {
     rules: [
       {
@@ -38,12 +47,15 @@ module.exports = {
         use: ["style-loader", "css-loader", "less-loader"]
       },
       {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-          },
-        ],
+        test: /\.(png|jpe?g|gif|bmp|eot|woff|woff2|ttf|svg)$/i,
+        use: {
+          loader: 'file-loader',
+          options: {
+            //placeholder 占位符
+            name: '[name].[ext]',
+            limit: 2048 //如果图片大小大于2048【2kb】字节的话，那就像file-loader下被打包到dist/images下，//否则会被打包成Base64的字符串放在bundle.js下
+          }
+        }
       },
     ],
   },
--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './assets/css/main.less'
+import avatar from './assets/img/avatar.jpg'
 // import { Button } from 'antd'
 
 export default () => (
   <>
     <div>
       <p>React</p>
+      <img src={avatar} alt=""/>
       {/* <Button>按钮</Button> */}
     </div>
     {/* <Button>React</Button> */}
