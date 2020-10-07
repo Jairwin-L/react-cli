beb3c7fa64e9f4af3e095ef5db286b7c828cbbe1
Jairwin <jairwin@jairwin.cn>
2020-10-07 22:35:23 +0800

    webpack

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './app'
-import { ConfigProvider } from 'antd'
+import {AppContainer} from 'react-hot-loader';
+import {ConfigProvider} from 'antd'
 import zhCN from 'antd/es/locale/zh_CN'
 import 'moment/locale/zh-cn'
+import App from './app'
 import '@less/common'
 
-ReactDOM.render(
-	<ConfigProvider locale={zhCN}>
-		<App />
-	</ConfigProvider>,
-	document.getElementById('app')
-)
+const render = Component => {
+  ReactDOM.render(
+    <ConfigProvider locale={zhCN}>
+      <AppContainer>
+        <Component />
+      </AppContainer>
+    </ConfigProvider>,
+    document.getElementById('app'),
+  );
+};
+
+render(App);
+
+// webpack Hot Module Replacement API
+if (module.hot) {
+  // keep in mind - here you are configuring HMR to accept CHILDREN MODULE
+  // while `hot` would configure HMR for the CURRENT module
+  module.hot.accept('./app', () => {
+    // if you are using harmony modules ({modules:false})
+    render(App);
+    // in all other cases - re-require App manually
+    render(require('./app'));
+  });
+}
